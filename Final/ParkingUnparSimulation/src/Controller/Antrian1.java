46dca05fbab5bdecbd48b996027957f5c8e557d9
Farhan Fariz Fazrian <farhanfazrian@gmail.com>
2018-05-30 20:40:04 +0700

    Update Fix

--- Final/ParkingUnparSimulation/src/Controller/Antrian1.java
+++ Final/ParkingUnparSimulation/src/Controller/Antrian1.java
@@ -1,14 +1,241 @@
+package Contoller;
+
+import Interface.OpenFile;
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controller;
-
 /**
  *
- * @author SAN
+ * Kelas yang mensimulasikan perhitungan waktu pada saat masuk parkiran motor
+ * PPAG UNPAR.
+ *
+ * @author Modsim Team
+ * @version 28 Mei 2018
+ *
  */
-public class Antrian1 {
+public class Antrian1 extends Machine {
+
+    //private int uang;
+    private int time; // dalam menit
+    //private boolean dompet; // kondisi kalo dompet ada di tas atau tidak
+
+    /**
+     * Variable untuk para pengendara. Disimpan dalam array merepresentasikan
+     * setiap indexnya adalah nomor pelanggan.
+     */
+    private int[] uang;
+    private int[] service;
+    private int[] arrival;
+    private int[] delay;
+    private int[] waiting;
+    private int[] completion;
+    private OpenFile of;
+    private int rangeData;
+    private int index;
+    private String result;
+    private int currentArrival;
+
+    public Antrian1() {
+
+        this.of = new OpenFile();
+
+        of.bacaInput();
+        this.rangeData = of.getArival().length;
+        int range = of.getArival().length;
+        
+        
+        this.uang = new int[range];
+        this.service = new int[range];
+        this.arrival = new int[range];
+        this.delay = new int[range];
+        this.waiting = new int[range];
+        this.completion = new int[range];
+
+        this.time = 1; // asumsi
+
+        this.generateUang();
+    }
+
+    private void generateUang() {
+        String[] uang = this.of.getUang();
+        for (int i = 0; i < rangeData; i++) {
+            String temp = of.getUang()[i];
+          
+            this.uang[i] = Integer.parseInt(temp);
+        }
+    }
+
+    @Override
+    /**
+     * To simulate the parking system.
+     */
+    public void proses() {
+
+        for (int i = 0; i < this.rangeData; i++) {
+
+            // CALCULATE ARRIVAL
+            this.calculateArrival();
+
+            // CALCULATE SERVICE
+            this.calculateService(1);
+
+            // UPDATE INDEX
+            this.updateIndex();
+
+            // CALCULATE DELAY       
+            //karena yang datang pertama itu tidak ada delay
+            if (i == 0) {
+                this.delay[i] = 0;
+            } else {
+                this.delay[i] = this.completion[i - 1] - this.arrival[i];
+            }
+
+            // CACULATE WAIT
+            this.waiting[i] = this.delay[i] + this.service[i];
+
+            // CALCULATE COMPLETION
+            this.completion[i] = this.arrival[i] + this.service[i] + this.delay[i];
+        }
+
+        this.variableToString();
+
+        this.printOut();
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void calculateService(int m) {
+        int i = this.index;
+        // CALCULATE SERVICE
+        // kondisi tidak pakai uang pas
+        if (this.uang[i] == 0) {
+            this.service[i] = this.time + 5;
+        } else {
+            this.service[i] = this.time + 2;
+        }
+    }
+
+    /**
+     * Generate sample case for arrival.
+     */
+    public void calculateArrival() {
+        Random r = new Random();
+        this.currentArrival = 0;
+        int i = this.index;
+
+        //arrival
+        
+        int randArrival = Integer.parseInt(of.getArival()[i]);
+        this.arrival[i] = this.currentArrival + randArrival;
+        //System.out.print(this.arrival[i]+"//");
+
+        //updating the arrival
+        this.currentArrival = this.currentArrival + randArrival;
+    }
+
+    /**
+     * Updating the index.
+     */
+    private void updateIndex() {
+        this.index++;
+    }
     
+    private String finalCount(){
+        double avService = 0.0;
+        for (int i = 0; i < this.rangeData; i++) {
+            avService += this.service[i];
+        }
+        avService = avService / this.rangeData;
+        
+        double avDelay = 0.0;
+        for (int i = 0; i < this.rangeData; i++) {
+            avDelay += this.delay[i];
+        }
+        avDelay = avDelay / this.rangeData;
+                
+        double avWait = 0.0;
+        for (int i = 0; i < this.rangeData; i++) {
+            avWait += this.waiting[i];
+        }
+        avWait = avWait / this.rangeData;
+        
+        String res = new String();
+        res += "\n";
+        res += "Average Service = ";
+        res += String.format("%.2f", avService) + " sec \n";
+        
+        res += "Average Delay = ";
+        res += String.format("%.2f", avDelay) + " sec \n";
+        
+        res += "Average Wait = ";
+        res += String.format("%.2f", avWait) + " sec \n";
+        
+        res += "Complete in "+this.completion[this.completion.length-1]+" sec";
+        
+        return res;
+    }
+
+    public void variableToString() {
+        this.result = new String();
+        String temp = new String();
+
+        //ARRIVAL 
+        this.result = "a: ";
+        for (int i = 0; i < this.rangeData; i++) {
+            temp += this.arrival[i] + ",";
+        }
+        temp = temp.substring(0, temp.length() - 1);
+        this.result += temp + "\n";
+        //System.out.println(this.result+"A");
+        temp = new String();
+
+        //SERVICE
+        this.result += "s: ";
+        for (int i = 0; i < this.rangeData; i++) {
+            temp += this.service[i] + ",";
+        }
+        temp = temp.substring(0, temp.length() - 1);
+        this.result += temp + "\n";
+        //System.out.println(temp+"S");
+        temp = new String();
+
+        //DELAY
+        this.result += "d: ";
+        for (int i = 0; i < this.rangeData; i++) {
+            temp += this.delay[i] + ",";
+        }
+        temp = temp.substring(0, temp.length() - 1);
+        this.result += temp + "\n";
+        temp = new String();
+
+        //WAIT
+        this.result += "w: ";
+        for (int i = 0; i < this.rangeData; i++) {
+            temp += this.waiting[i] + ",";
+        }
+        temp = temp.substring(0, temp.length() - 1);
+        this.result += temp + "\n";
+        temp = new String();
+
+        //COMPLETION
+        this.result += "c: ";
+        for (int i = 0; i < this.rangeData; i++) {
+            temp += this.completion[i] + ",";
+        }
+        temp = temp.substring(0, temp.length() - 1);
+        this.result += temp + "\n";
+
+        this.result += this.finalCount();
+    }
+
+    public void printOut() {
+        System.out.println(this.result);
+    }
+
 }

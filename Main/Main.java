b0c041ac19c2d532110c9b8d9fc1da66126be7d8
Rendra96 <33146490+Rendra96@users.noreply.github.com>
2018-05-30 16:17:33 +0700

    Update

--- Main/Main.java
+++ Main/Main.java
@@ -5,10 +5,26 @@
  */
 package Main;
 
+import Contoller.AntriKeluar;
+import Contoller.AntriMasuk;
+import Contoller.Machine;
+
 /**
  *
  * @author SAN
  */
 public class Main {
+    public static void main(String[] args) {
+        Machine amt1 = new AntriMasuk(10);
+        Machine amt2 = new AntriKeluar(10);
     
+        amt1.proses();
+        amt2.proses();
+        
+        System.out.println("Simulasi antrian masuk parkiran motor PPAG UNPAR");
+        System.out.println(amt1.printOut());
+        System.out.println("---------------------------------------------------");
+        System.out.println("Simulasi antrian keluar parkiran motor PPAG UNPAR");
+        System.out.println(amt2.printOut());
+    }
 }
